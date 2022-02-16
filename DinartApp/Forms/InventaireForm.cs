886460bbc8aabc9a99fdf75bf01d99f0775f9847
Snailboy <meunierdormant@gmail.com>
2022-02-16 17:12:34 +0100

    experiment excel cells modifier

--- DinartApp/Forms/InventaireForm.cs
+++ DinartApp/Forms/InventaireForm.cs
@@ -14,47 +14,69 @@ namespace DinartApp.Forms
 {
     public partial class InventaireForm : Form
     {
+        private Microsoft.Office.Interop.Excel.Application xlApp;
+        private Microsoft.Office.Interop.Excel.Workbook xlWorkbook;
+        private Microsoft.Office.Interop.Excel.Worksheet xlWorksheet;
+        private Microsoft.Office.Interop.Excel.Range xlRange;
+
+        private int xlRow;
+
         public InventaireForm()
         {
             InitializeComponent();
-            readExcel();
+            OpenExcelFile();
+            ReadExcel();
+            CloseExcelFile();
         }
-
-        private void readExcel()
+        public void OpenExcelFile()
         {
             var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string localPath = new Uri(outPutDirectory).LocalPath;
             string[] strSplitPath = localPath.Split(new String[] { "bin" }, StringSplitOptions.None);
             string strFilePath = strSplitPath[0] + "Ressources\\Inventaire.xlsx";
 
-            /*
-            var filePath = Path.Combine(outPutDirectory, "Ressources\\Inventaire.xslx");
-            string strFilePath = new Uri(filePath).LocalPath;
-            */
-            Microsoft.Office.Interop.Excel.Application xlApp;
-            Microsoft.Office.Interop.Excel.Workbook xlWorkbook;
-            Microsoft.Office.Interop.Excel.Worksheet xlWorksheet;
-            Microsoft.Office.Interop.Excel.Range xlRange;
+            this.xlApp = new Microsoft.Office.Interop.Excel.Application();
+            this.xlWorkbook = xlApp.Workbooks.Open(strFilePath); // chemin vers fichier
+            this.xlWorksheet = xlWorkbook.Worksheets[1];
+            this.xlRange = xlWorksheet.UsedRange;
+        }
 
-            int xlRow;
-
-            xlApp = new Microsoft.Office.Interop.Excel.Application();
-            xlWorkbook = xlApp.Workbooks.Open(strFilePath); // chemin vers fichier
-            xlWorksheet = xlWorkbook.Worksheets[1];
-            xlRange = xlWorksheet.UsedRange;
-
-            for (xlRow = 4; xlRow <= xlRange.Rows.Count; xlRow++)
+        public void ReadExcel()
+        {
+            for (this.xlRow = 4; this.xlRow <= this.xlRange.Rows.Count; this.xlRow++)
             {
-                if (xlRange.Cells[xlRow, 1].Text != "")
+                if (this.xlRange.Cells[xlRow, 1].Text != "")
                 {
                     dgvInventaire.Rows.Add(
-                        xlRange.Cells[xlRow, 1].Text,
-                        xlRange.Cells[xlRow, 2].Text
+                        this.xlRange.Cells[xlRow, 1].Text,
+                        this.xlRange.Cells[xlRow, 2].Text
                     ); // 1 = A, 2 = B, ...
                 }
             }
-            xlWorkbook.Close();
-            xlApp.Quit();
+        }
+
+        public void WriteExcel(string newValue)
+        {
+            OpenExcelFile();
+
+            for (this.xlRow = 4; this.xlRow <= this.xlRange.Rows.Count; this.xlRow++)
+            {
+                if (this.xlRange.Cells[xlRow, 1].Text != "" 
+                    && this.xlRange.Cells[xlRow+1, 1].Text != "")
+                {
+                    break;
+                }
+                this.xlRange.Cells[xlRow, 1].Value = dgvInventaire.CurrentCell.Value;
+                this.xlRange.Cells[xlRow, 2].Value = dgvInventaire.CurrentCell.Value;
+            }
+
+            CloseExcelFile();
+        }
+
+        public void CloseExcelFile()
+        {
+            this.xlWorkbook.Close();
+            this.xlApp.Quit();
         }
     }
 }
